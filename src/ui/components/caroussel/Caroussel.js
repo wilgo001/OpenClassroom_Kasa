576e57daf2420ed00585aea254d544a15a24e67c
wilgo001 <wilhelm.salles@outlook.fr>
2022-05-13 17:30:07 +0200

    remove unused code

--- src/ui/components/caroussel/Caroussel.js
+++ src/ui/components/caroussel/Caroussel.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './Caroussel.css';
 
 function Caroussel(props) {
